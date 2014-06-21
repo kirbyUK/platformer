fd544dd24dfe337b472091a33bdaf651b1fa9ec7
Alex Kerr <kirbyman62@gmail.com>
2014-06-21 17:12:13 +0100

    I'm only committing so often because I keep switching computers

---
+++ block.h
@@ -0,0 +1,27 @@
+/* Copyright (c) Alex Kerr 2014
+* This file is part of 'platformer'
+*
+* 'platformer' is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* 'platformer' is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRENTY; without the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Purpose License
+* along with 'platformer'. If not, see <http://www.gnu.org/licenses/>.
+*/
+#ifndef BLOCK_H
+#define BLOCK_H
+#include <SFML/Graphics.hpp>
+
+class Block
+{
+	private:
+		sf::RectangleShape _shape;
+};
+
+#endif
