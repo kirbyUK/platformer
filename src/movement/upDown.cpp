01f8916fbeb0fdca3f92a79b574989835898b6ed
Alex Kerr <kirbyman62@gmail.com>
2014-11-03 21:30:11 +0000

    Updated license to ISC

--- src/movement/upDown.cpp
+++ src/movement/upDown.cpp
@@ -1,18 +1,16 @@
 /* Copyright (c) Alex Kerr 2014
 * This file is part of 'platformer'
+* Permission to use, copy, modify, and/or distribute this software for any
+* purpose with or without fee is hereby granted, provided that the above
+* copyright notice and this permission notice appear in all copies.
 *
-* 'platformer' is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* 'platformer' is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRENTY; without the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Purpose License
-* along with 'platformer'. If not, see <http://www.gnu.org/licenses/>.
+* THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+* WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+* MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+* SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+* WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+* OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+* CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
 #include "upDown.h"
 
